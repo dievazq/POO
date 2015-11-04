475b080474d52382bac093d7224f04c2a7fcd2be
Diego Vázquez Blanco <oxxixo@gmail.com>
2015-11-04 01:09:27 +0100

    Update Matricula.java

--- dievazq-rodpere/src/academia/Matricula.java
+++ dievazq-rodpere/src/academia/Matricula.java
@@ -8,21 +8,14 @@ public class Matricula {
 	private Curso curso;
 	private Boolean pagado;
 	
-	private ArrayList<Matricula> matriculas = new ArrayList<Matricula>();
-	
 	private ArrayList<Matricula> matriculas_sin_pagar = new ArrayList<Matricula>();
 
 	public Matricula(int numero, Alumno alumno, Curso curso) {
 		
-		// Comprueba si ya existe un alumno en el curso y si supera el max de alumnos
-		if (comprobarDuplicado(alumno, curso) == false && comprobarMaxAlumnos(curso) == false) {
 			this.num = numero;
 			this.alumno = alumno;
 			this.curso = curso;
 			pagado = false;
-		}
-		else
-			System.err.println("No se ha podido crear la matricula.");
 	}
 	
 	public Matricula() {
@@ -42,63 +35,21 @@ public class Matricula {
 		return pagado;
 	}
 	
+	public ArrayList<Matricula> getMatriculasSinPagar() {
+		
+		Academia academia = new Academia();
+		
+		for (int i=0; i < academia.getMatriculas().size(); i++) {
+			if (academia.getMatriculas().get(i).getPagado() == false)
+				matriculas_sin_pagar.add(academia.getMatriculas().get(i));
+		}
+		return matriculas_sin_pagar;
+	}
+	
 
 	// SETTERS
 	public void setPagado(Boolean pagado) {
 		this.pagado = pagado;
-	}
-
-	
-	
-	public Boolean comprobarDuplicado(Alumno alumno, Curso curso) {
-		
-		for (int i=0; i < curso.getAlumnos(curso.getID()).size(); i++) {
-			if (alumno.getDNI() == curso.getAlumnos(curso.getID()).get(i).getDNI())
-				return true;
-		}
-		return false;
-	}
-	
-	public Boolean comprobarMaxAlumnos(Curso curso) {
-		
-		if ( (curso.getNumAlumnos(curso.getID()) + 1) <= curso.getNumMaxAlumnos() )
-			return false;
-		else
-			return true;
-	}
-			
-	public void cambiarnivel(Alumno alumno, Curso curso, int nivel){
-		
-		if (nivel == (curso.getNivel() + 1) || nivel == (curso.getNivel() - 1) &&
-				comprobarMaxAlumnos(curso) == false)
-			curso.setNivel(nivel);
-	}
-	
-	public void anadirMatriculas(ArrayList<Matricula> matriculas) {
-		
-		matriculas.addAll(matriculas);
-	}
-	
-	public void pagarMatricula(Alumno alumno, Curso curso) {
-		
-		for (int i=0; i < matriculas.size(); i++) {
-			if ( (alumno.getDNI() == matriculas.get(i).getAlumno().getDNI()) && 
-					(curso.getID() == matriculas.get(i).getCurso().getID()) )
-				matriculas.get(i).setPagado(true);
-		}
-	}
-	
-	public ArrayList<Matricula> getMatriculas() {
-		return matriculas;
-	}
-	
-	public ArrayList<Matricula> getMatriculasSinPagar() {
-		
-		for (int i=0; i < matriculas.size(); i++) {
-			if (matriculas.get(i).getPagado() == false)
-				matriculas_sin_pagar.add(matriculas.get(i));
-		}
-		return matriculas_sin_pagar;
 	}
 	
 }
